4adcf66c05417916e70807ced7dc12ff805214bc
Jkerr6 <122476114+Jkerr6@users.noreply.github.com>
2023-04-13 17:04:15 -0400

    Updates to thermometre and some prelim for final project

---
+++ lib2024/Assignment 4/js/scripts.js
@@ -0,0 +1,14 @@
+/* This variable detects our input. */
+let tempInput;
+
+/* This variable will be our output */
+let tempOutput;
+
+/* This variable is to detect what we are converting to. */
+let whatUnit;
+
+/* Here I am going to create a function to fill in the converted temperature */
+
+function ConvertTemp(input) {
+
+}
